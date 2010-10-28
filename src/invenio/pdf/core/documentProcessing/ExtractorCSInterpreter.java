abc36b110b0450bd3bf956a3c33b91c7f14987bd
unknown <piotr@.(none)>
2010-10-28 11:28:50 +0200

    Small commit - mostly to move the code between machines:
    * extraction of string representation of text operators (Very imperfect yet)

--- src/invenio/pdf/core/documentProcessing/ExtractorCSInterpreter.java
+++ src/invenio/pdf/core/documentProcessing/ExtractorCSInterpreter.java
@@ -4,7 +4,12 @@ import java.util.Map;
 import de.intarsys.cwt.environment.IGraphicsContext;
 import de.intarsys.pdf.content.CSException;
 import de.intarsys.pdf.content.CSOperation;
+import de.intarsys.pdf.cos.COSArray;
+import de.intarsys.pdf.cos.COSObject;
+import de.intarsys.pdf.cos.COSString;
 import de.intarsys.pdf.platform.cwt.rendering.CSPlatformRenderer;
+import java.util.Iterator;
+import java.util.List;
 
 class ExtractorCSInterpreter extends CSPlatformRenderer {
 
@@ -33,24 +38,58 @@ class ExtractorCSInterpreter extends CSPlatformRenderer {
     @Override
     protected void render_DoubleQuote(CSOperation operation) throws CSException {
         this.operationsManager.addTextOperation(operation);
+        extractTextOperationString(operation);
         super.render_DoubleQuote(operation);
     }
 
     @Override
     protected void render_Quote(CSOperation operation) throws CSException {
         this.operationsManager.addTextOperation(operation);
+        extractTextOperationString(operation);
         super.render_Quote(operation);
     }
 
     @Override
     protected void render_Tj(CSOperation operation) throws CSException {
         this.operationsManager.addTextOperation(operation);
+        extractTextOperationString(operation);
         super.render_Tj(operation);
     }
 
     @Override
     protected void render_TJ(CSOperation operation) throws CSException {
         this.operationsManager.addTextOperation(operation);
+        extractTextOperationString(operation);
         super.render_TJ(operation);
+    }
+
+    /**
+     * Text is passed to text operations in more or less standard manner - this 
+     * function tries to extract this text in a geenral manner
+     * @param operation
+     * @return
+     */
+    private String extractTextOperationString(CSOperation operation) {
+        // interpreting the arguments -> reading the spring
+        Iterator<COSObject> operandsIter = operation.getOperands();
+        COSObject firstOperand = null;
+        if (operandsIter.hasNext()) {
+            firstOperand = operandsIter.next();
+        }
+
+        if (firstOperand instanceof COSArray) {
+            COSArray operandsArray = (COSArray) firstOperand;
+            for (COSObject operand : (List<COSObject>) operandsArray.getObjects()) {
+                if (operand instanceof COSString) {
+                    COSString stringArg = (COSString) operand;
+                    System.out.print(stringArg.getValueString(""));
+                }
+            }
+        } else if (firstOperand instanceof COSString){
+            COSString arg = (COSString) firstOperand;
+            System.out.print(arg.getValueString(""));
+        }
+        System.out.print(" ");
+        return "";
     }
 }
