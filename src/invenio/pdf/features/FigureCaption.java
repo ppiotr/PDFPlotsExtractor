1566cc11c261631aae8c69f6683e93c7955e6f53
Piotr Praczyk <piotr.praczyk@gmail.com>
2012-06-27 00:45:55 +0200

    Forgotten file

---
+++ src/invenio/pdf/features/FigureCaption.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invenio.pdf.features;
+
+import java.awt.Rectangle;
+
+/**
+ * A class descibing figure caption extracted from PDF
+ * @author piotr
+ */
+public class FigureCaption {
+    public String text; // the text of the caption
+    public Rectangle boundary; // boundary inside of a page rectangle
+    public boolean isTable; // Does this caption describe table ? 
+    public String figureIdentifier; // Figure identifier derieved from the caption
+    public boolean alreadyMatched; // has this caption been already matched with a figure ? 
+}
