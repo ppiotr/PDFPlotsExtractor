cc0cd908997ee505aa01637afb393766b5416957
Piotr Praczyk <piotr.praczyk@gmail.com>
2013-03-02 22:24:34 +0100

    Some new util functions manipulating rectangles

--- src/main/java/invenio/common/ExtractorGeometryTools.java
+++ src/main/java/invenio/common/ExtractorGeometryTools.java
@@ -7,12 +7,14 @@ package invenio.common;
 import java.awt.Rectangle;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * A set of methods implementing commong geometric operations that can be used
  * across the application
+ *
  * @author piotr
  */
 public class ExtractorGeometryTools {
@@ -23,9 +25,9 @@ public class ExtractorGeometryTools {
      *
      * @param original original rectangle ro be extended
      * @param hMargin number by which the rectangle should grow into the left
-     *                and into the right
+     * and into the right
      * @param vMargin number by which the original rectangle should grow to the
-     *                top and to the bottom
+     * top and to the bottom
      * @return a new rectangle
      */
     public static Rectangle extendRectangle(Rectangle original,
@@ -38,6 +40,7 @@ public class ExtractorGeometryTools {
 
     /**
      * Shrinks a given rectangle by margin sizes passed as arguments
+     *
      * @param original rectangle to be modified
      * @param hmargin margins in horizontal directions
      * @param vMargin margins in vertical directions
@@ -53,8 +56,9 @@ public class ExtractorGeometryTools {
 
     /**
      * Shrink all the keys of a dictionary indexed by rectangles
+     *
      * @param <T> some type constituting a value of the mapping, we do not
-     *            assume anything about it
+     * assume anything about it
      * @param originals
      * @param hMargin
      * @param vMargin
@@ -72,7 +76,7 @@ public class ExtractorGeometryTools {
     /**
      * Cropping in one dimension
      *
-     * @param pos the value to be cropped 
+     * @param pos the value to be cropped
      * @param bp boundary beginning value
      * @param bl boundary extent
      * @return
@@ -81,8 +85,9 @@ public class ExtractorGeometryTools {
         return (pos < bp) ? bp : ((pos > bp + bl) ? bp + bl : pos);
     }
 
-    /** Cropp the rectangle rec so that it is equal to the intersection of
-     * rec and bd
+    /**
+     * Cropp the rectangle rec so that it is equal to the intersection of rec
+     * and bd
      *
      * @param rec The rectangle to crop
      * @param bd The boundary limit rectangle
@@ -97,22 +102,52 @@ public class ExtractorGeometryTools {
         int maxy = cropPoint(rec.y + rec.height, bd.y, bd.height);
         return new Rectangle(x, y, maxx - x, maxy - y);
     }
+
     /**
      * Preapre a Point2D instace for display
+     *
      * @param point
-     * @return 
+     * @return
      */
-    public static String pointToString(Point2D point){
-        return "(" + point.getX() + ", " + point.getY() + ")";              
+    public static String pointToString(Point2D point) {
+        return "(" + point.getX() + ", " + point.getY() + ")";
     }
-    
+
     /**
      * Prepare a Line2D for the display
+     *
      * @param line
-     * @return 
+     * @return
      */
-    public static String lineToString(Line2D line){
+    public static String lineToString(Line2D line) {
         return "(" + pointToString(line.getP1()) + ", " + pointToString(line.getP2()) + ")";
     }
-    
+
+    /**
+     * Multiplies all points of the rectangle by the scale coefficient and then
+     * rounds all the dimensions to integers, returning a regular Rectangle
+     * instance
+     *
+     * @param rec
+     * @param scale
+     * @return
+     */
+    public static Rectangle roundScaleRectangle(Rectangle2D rec, double scale) {
+        int x = (int) Math.round(rec.getMinX() * scale);
+        int y = (int) Math.round(rec.getMinY() * scale);
+        int width = (int) Math.round(rec.getWidth() * scale);
+        int height = (int) Math.round(rec.getHeight() * scale);
+        return new Rectangle(x, y, width, height);
+    }
+
+    /**
+     * Rounds all the coordinates of the rectangle to integers and returns a
+     * regular Rectangle instance
+     *
+     * @param rec
+     * @return
+     */
+    public static Rectangle roundRectangle(Rectangle2D rec) {
+        return ExtractorGeometryTools.roundScaleRectangle(rec, 1);
+    }
 }
